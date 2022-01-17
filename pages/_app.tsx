b6f56d89a661ac4802d782111b4c3abcdcd481ae
alexc0794 <alexchou94@gmail.com>
2022-01-17 17:54:36 -0500

    Update icon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import '../styles/globals.css'
 import { ReactElement } from 'react';
 import { Provider as ReduxProvider } from 'react-redux';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import { ChakraProvider } from '@chakra-ui/react';
 import theme from "../theme";
 import { store } from '../store';
@@ -10,13 +11,22 @@ import NavBar from '../components/NavBar';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ReduxProvider store={store}>
-      <ChakraProvider theme={theme}>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </ChakraProvider>
-    </ReduxProvider>
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#000000" />
+        <meta name="description" content="Cobyo Meetup web app" />
+        <link rel="icon" href="/image/favicon.ico" />
+      </Head>
+      <ReduxProvider store={store}>
+        <ChakraProvider theme={theme}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </ChakraProvider>
+      </ReduxProvider>
+    </>
   );
 }
 
