d0b2e70d6b967db69cb286802fe9c89bff6701d1
alexc0794 <alexchou94@gmail.com>
2022-01-17 20:13:19 -0500

    Install next-redux-wrapper

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,31 +1,35 @@
 import '@/styles/globals.css'
-import { ReactElement } from 'react';
-import { Provider as ReduxProvider } from 'react-redux';
+import { Component, ReactElement } from 'react';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import { createWrapper } from 'next-redux-wrapper';
 import { ChakraProvider } from '@chakra-ui/react';
 import theme from "../theme";
 import { store } from '../store';
 import NavBar from '@/components/NavBar';
 
 
-export default function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <ReduxProvider store={store}>
-      <Head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="theme-color" content="#000000" />
-        <meta name="description" content="Cobyo Meetup web app" />
-        <link rel="icon" href="/image/favicon.ico" />
-      </Head>
-      <ChakraProvider theme={theme}>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </ChakraProvider>
-    </ReduxProvider>
-  );
+class MyApp extends Component<AppProps> {
+  render() {
+    const { Component, pageProps } = this.props;
+    return (
+      <>
+        <Head>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta name="theme-color" content="#000000" />
+          <meta name="description" content="Cobyo Meetup web app" />
+          <link rel="icon" href="/image/favicon.ico" />
+        </Head>
+        <ChakraProvider theme={theme}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </ChakraProvider>
+      </>
+    );
+  }
+
 }
 
 
@@ -37,3 +41,8 @@ const Layout = ({ children }: { children: ReactElement }) => {
     </div>
   );
 }
+
+const makeStore = () => store;
+const wrapper = createWrapper(makeStore);
+
+export default wrapper.withRedux(MyApp);
