6eb8248b7579ab59607b63adb1491c30fd6c6aa5
alexc0794 <alexchou94@gmail.com>
2022-01-19 18:49:19 -0500

    Check if token is expired

--- src/session/redux/selectors.ts
+++ src/session/redux/selectors.ts
@@ -2,7 +2,12 @@ import { createSelector } from '@reduxjs/toolkit';
 import { RootState } from 'src/store';
 
 
-export const selectToken = (state: RootState) => state.session.user?.sessionToken?.token || null;
+export const selectToken = (state: RootState) => {
+  const sessionToken = state.session.user?.sessionToken;
+  return sessionToken && sessionToken.tokenExpiresAtMs > Date.now() 
+    ? sessionToken.token 
+    : null;
+}
 export const selectPhoneNumber = (state: RootState) => state.session.user?.phoneNumber || null;
 
 export const tokenSelector = createSelector(selectToken, state => state);
